4fea639c1cdf536539819a1ad8df499061b2afb7
Neil Bateman <neilbatman@gmail.com>
2019-09-06 10:55:31 -0700

    create fake db to display kegs

---
+++ src/kegstore.js
@@ -0,0 +1,53 @@
+export const kegstore = [
+    {
+        type: 'Beer',
+        brand: 'Tecate',
+        pints: 100,
+        price: 2,
+        abv: 5
+    },
+    {
+        type: 'Cider',
+        brand: 'Angry Orchard',
+        pints: 200,
+        price: 2,
+        abv: 6
+    },
+    {
+        type: 'Beer',
+        brand: 'Modelo',
+        pints: 100,
+        price: 2,
+        abv: 5
+    },
+    {
+        type: 'Tequila',
+        brand: 'Mescale',
+        pints: 100,
+        price: 50,
+        abv: 40
+    },
+    {
+        type: 'Beer',
+        brand: 'Tecate',
+        pints: 100,
+        price: 2,
+        abv: 5
+    },
+    {
+        type: 'Beer',
+        brand: 'Tecate',
+        pints: 100,
+        price: 2,
+        abv: 5
+
+    },
+    {
+        type: 'Beer',
+        brand: 'Tecate',
+        pints: 100,
+        price: 2,
+        abv: 5
+
+    },
+]
