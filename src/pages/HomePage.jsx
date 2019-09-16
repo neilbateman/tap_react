6ac5563806d20d4c89c71c09afc0e5102082b64f
Neil Bateman <neilbatman@gmail.com>
2019-09-15 21:57:05 -0700

    changed background image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,21 +6,23 @@ import beer from '../assets/beer.svg';
 import 'typeface-roboto';
 import '../App.css';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    textAlign: 'center',
-    width: '100%',
-    maxWidth: 500,
+    flexGrow: 1,
   },
-});
+  paper: {
+    textAlign: 'center',
+    color: 'lightblue',
+  },
+}));
 
 const Home = () => {
   const classes = useStyles();
         return (
           <div className="App">
           <UserGreeting/>
-            <Typography variant="h3" component="h2" gutterBottom>
-              Welcome to the React Tap Room!
+            <Typography className={classes.paper}>
+              <h2>React Tap Room!</h2>
             </Typography><br/><br/>
             <img src={beer} className="App-logo" alt="logo" />
           </div>
